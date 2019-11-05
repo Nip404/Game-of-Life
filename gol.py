55162605612d9e2001042f3770918792b0e9aedf
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 23:10:33 +0000

    Update and rename main.py to gol.py

--- gol.py
+++ gol.py
@@ -2,27 +2,27 @@ import engine
 import pygame
 import sys
 
-s = [500,500]
-res = [50,50]
+s = [500, 500]
+res = [50, 50]
 
 pygame.init()
-screen = pygame.display.set_mode(s,0,32)
+screen = pygame.display.set_mode(s, 0, 32)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Conway's Game of Life")
 
 def menu():
-    h1 = pygame.font.SysFont("Garamond MS",40)
+    h1 = pygame.font.SysFont("Garamond MS", 40)
     
-    box1 = pygame.Rect(100,100,300,100)
-    box2 = pygame.Rect(100,300,300,100)
+    box1 = pygame.Rect(100, 100, 300, 100)
+    box2 = pygame.Rect(100, 300, 300, 100)
 
-    txt1 = h1.render("Generate Random",True,(0,0,0))
-    txt2 = h1.render("Custom",True,(0,0,0))
+    txt1 = h1.render("Generate Random", True, (0, 0, 0))
+    txt2 = h1.render("Custom", True, (0, 0, 0))
 
-    onHover = (100,250,20)
+    onHover = (100, 250, 20)
 
     while True:
-        mouse = pygame.Rect([i-1 for i in pygame.mouse.get_pos()],[2,2])
+        mouse = pygame.Rect([i-1 for i in pygame.mouse.get_pos()], [2, 2])
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -36,18 +36,18 @@ def menu():
                 elif mouse.colliderect(box2):
                     return 1
 
-        screen.fill((255,255,255))
-        pygame.draw.rect(screen,(255,255,255) if not mouse.colliderect(box1) else onHover,box1,0)
-        pygame.draw.rect(screen,(255,255,255) if not mouse.colliderect(box2) else onHover,box2,0)
-        pygame.draw.rect(screen,(0,0,0),box1,1)
-        pygame.draw.rect(screen,(0,0,0),box2,1)
-        screen.blit(txt1,txt1.get_rect(center=box1.center))
-        screen.blit(txt2,txt2.get_rect(center=box2.center))
+        screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, (255, 255, 255) if not mouse.colliderect(box1) else onHover, box1, 0)
+        pygame.draw.rect(screen, (255, 255, 255) if not mouse.colliderect(box2) else onHover, box2, 0)
+        pygame.draw.rect(screen, (0, 0, 0), box1, 1)
+        pygame.draw.rect(screen, (0, 0, 0), box2, 1)
+        screen.blit(txt1, txt1.get_rect(center=box1.center))
+        screen.blit(txt2, txt2.get_rect(center=box2.center))
 
         pygame.display.flip()
 
 def main():
-    board = engine.Board(screen,res)
+    board = engine.Board(screen, res)
     board.generate(menu())
 
     paused = False
